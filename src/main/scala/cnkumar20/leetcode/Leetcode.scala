025bce5a77d524d9dc9c9507a8daf7d73c56f609
Nandiesh Kumar Chandregowda <nanchand@WKMZTE5C79D8.global.publicisgroupe.net>
2021-09-10 02:22:31 -0500

    twoSum

--- src/main/scala/cnkumar20/leetcode/Leetcode.scala
+++ src/main/scala/cnkumar20/leetcode/Leetcode.scala
@@ -1,45 +1,44 @@
 package cnkumar20.leetcode
-
+import scala.collection.mutable.ListBuffer
 class Leetcode {
 
-  def problemStatement(problem:Int):String = {
+  def problemStatement(problem: Int): String = {
     problem match {
-      case 1=> println(1)
-      case _=> println(2)
+      case 1 => println(1)
+      case _ => println(2)
     }
     return "executed"
   }
 
-  def maxPosition(timeList:Array[Int]):Array[Int] = {
+  def maxPosition(timeList: Array[Int]): Array[Int] = {
     //time = (9,29,39,59,60)
     //keys = (abcde)
     var tempArray = new Array[Int](timeList.length)
     var beg = 0
-    for(i <- 0 to tempArray.length) {
+    for (i <- 0 to tempArray.length) {
       tempArray(i) = timeList(i) - beg
       beg = timeList(i)
     }
     val pos = tempArray.zipWithIndex.filter(_._1 == tempArray.max)
     pos.map(_._2.toInt).toArray
   }
-  def LongestKeyPressed(inputKeys:List[Char],timeList:Array[Int]):Char = {
-    val postArray =  maxPosition(timeList)
+
+  def LongestKeyPressed(inputKeys: List[Char], timeList: Array[Int]): Char = {
+    val postArray = maxPosition(timeList)
     val c = postArray.map(inputKeys(_))
     c.max
   }
+
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
     var sumMap = Map[Int,Int]()
-    nums.foreach(x => sumMap += (target-x -> x))
-    var res = Array[Int]()
-    for (x <- 0 to nums.size-1) {
-      println("Hello")
-      if(sumMap.contains(nums(x))) {
-        print("Kumar")
-        res = res:+x
+    for(x <- 0 to nums.size-1) {
+      if(!sumMap.contains(nums(x))) {
+        sumMap += (target-nums(x) -> x)
+      }
+      else {
+        return Array(sumMap(nums(x)),x)
       }
     }
-    res
+    return Array()
   }
-
-
 }
